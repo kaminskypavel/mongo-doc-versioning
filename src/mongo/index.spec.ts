f3be5408c1c826adb3e1bb44bbfb11fa0b5cd938
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-10-21 15:30:54 +0300

    refactor: make code better for future npm publish

--- src/mongo/index.spec.ts
+++ src/mongo/index.spec.ts
@@ -1,43 +1,9 @@
 import { MongoClient, Db, ObjectId } from "mongodb";
 import faker from "faker";
-import { getMongoUri } from "./utils/env";
+import { getMongoUri } from "../utils/env";
 import { MongoDb } from ".";
-import { MongoVersionedDocument } from "./types";
+import { MongoVersionedDocument } from "../types";
 
-describe("insert", () => {
-  let connection: MongoClient;
-  let db: Db;
-
-  beforeAll(async () => {
-    const { __MONGO_URI__ } = global as any;
-
-    connection = await MongoClient.connect(getMongoUri(), {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-    });
-    db = await connection.db();
-  });
-
-  it("should insert a doc into collection", async () => {
-    const users = db.collection("users");
-
-    const mockUser = {
-      _id: new ObjectId("some-user-id"),
-      commit: faker.git.commitMessage(),
-    };
-    await users.insertOne(mockUser);
-    console.log(mockUser);
-
-    const insertedUser = await users.findOne({
-      _id: new ObjectId("some-user-id"),
-    });
-    expect(insertedUser).toEqual(mockUser);
-  });
-
-  afterAll(async () => {
-    await connection.close();
-  });
-});
 
 describe("#MongoDb", () => {
   const mongoDb = new MongoDb(getMongoUri());
@@ -86,6 +52,7 @@ describe("#MongoDb", () => {
 
       expect(doc).toEqual(user);
     });
+
     it("should find a document and return it 'raw'", async () => {
       const { _id } = await mongoDb.insertDocument(user);
 
